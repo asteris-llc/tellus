50d3d5b4464c6c374e3c1a5e36a195fae66c86d7
Brian Hicks <brian@brianthicks.com>
2015-07-15 15:49:31 -0500

    storage(memory): change from "project" to "key"

--- storage/memory.go
+++ storage/memory.go
@@ -16,11 +16,11 @@ func NewMemoryStore() *MemoryStore {
 	}
 }
 
-func (ms *MemoryStore) Get(project string) (State, error) {
+func (ms *MemoryStore) Get(key string) (State, error) {
 	ms.sync.RLock()
 	defer ms.sync.RUnlock()
 
-	state, present := ms.store[project]
+	state, present := ms.store[key]
 	if !present {
 		return State{}, ErrNoProject
 	}
@@ -28,26 +28,26 @@ func (ms *MemoryStore) Get(project string) (State, error) {
 	return state, nil
 }
 
-func (ms *MemoryStore) Set(project string, state State) error {
+func (ms *MemoryStore) Set(key string, state State) error {
 	ms.sync.Lock()
 	defer ms.sync.Unlock()
 
-	ms.store[project] = state
+	ms.store[key] = state
 	return nil
 }
 
-func (ms *MemoryStore) Delete(project string) error {
+func (ms *MemoryStore) Delete(key string) error {
 	ms.sync.Lock()
 	defer ms.sync.Unlock()
 
 	// this isn't *strictly* necessary here because delete is a no-op if the
 	// project doesn't exist, but the rest of the stores should raise an error in
 	// this case so we do here too.
-	_, present := ms.store[project]
+	_, present := ms.store[key]
 	if !present {
 		return ErrNoProject
 	}
 
-	delete(ms.store, project)
+	delete(ms.store, key)
 	return nil
 }
--- storage/memory_test.go
+++ storage/memory_test.go
@@ -9,21 +9,21 @@ func TestMemoryStoreLifecycle(t *testing.T) {
 	t.Parallel()
 
 	ms := NewMemoryStore()
-	project := "test"
+	key := "test"
 	state := State("test")
 	// put and get
-	err := ms.Set(project, state)
+	err := ms.Set(key, state)
 	assert.Nil(t, err)
 
-	state2, err := ms.Get(project)
+	state2, err := ms.Get(key)
 	assert.Nil(t, err)
 	assert.Equal(t, state, state2)
 
 	// delete and get
-	err = ms.Delete(project)
+	err = ms.Delete(key)
 	assert.Nil(t, err)
 
-	state2, err = ms.Get(project)
+	state2, err = ms.Get(key)
 	assert.Equal(t, err, ErrNoProject)
 	assert.Equal(t, state2, State{})
 }
