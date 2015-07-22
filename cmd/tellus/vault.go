9f3d10cd2a89672eca221e20bd2550b4965afa46
Brian Hicks <brian@brianthicks.com>
2015-07-22 11:42:42 -0500

    cmd(tellus): add info logging to token resolution

--- cmd/tellus/vault.go
+++ cmd/tellus/vault.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/Sirupsen/logrus"
 	"github.com/hashicorp/vault/api"
 	"github.com/spf13/viper"
 	"io/ioutil"
@@ -19,7 +20,11 @@ func VaultClient() (*api.Client, error) {
 	token := viper.GetString("vault-token")
 	if token == "" {
 		token = tokenFromFilesystem()
+		logrus.Debug("read vault token from disk")
+	} else {
+		logrus.Debug("read vault token from config")
 	}
+	client.SetToken(token)
 
 	return client, nil
 }
@@ -27,12 +32,18 @@ func VaultClient() (*api.Client, error) {
 func tokenFromFilesystem() string {
 	usr, err := user.Current()
 	if err != nil { // no current user, no home directory!
+		logrus.Warn("no current user set, cannot read token from filesystem")
 		return ""
 	}
 
 	token, err := ioutil.ReadFile(usr.HomeDir + "/.vault-token")
 	if err != nil { // couldn't read it, oh well
+		logrus.WithField("error", err).Warn("could not read vault token from disk")
 		return ""
+	}
+
+	if len(token) == 0 {
+		logrus.Warn("empty vault token loaded")
 	}
 
 	return string(token)
